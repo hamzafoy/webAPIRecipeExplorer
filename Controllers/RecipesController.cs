68b6fe238bccef4e8de6e9a27386e48e9e810ffe
hamzafoy <hamzaoflouisville@gmail.com>
2022-12-14 16:22:50 -0500

    Add project files.

---
+++ Controllers/RecipesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIRecipes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipesController : ControllerBase
+    {
+        [HttpGet]
+
+        public string[] GetMeals()
+        {
+            string[] meals = { "Mujadarah", "Chicken Tikka", "Fuul Medammas", "Lamb Biryani", "Nihari", "Mansaaf", "Kofta Kebab" };
+            return meals;
+        }
+    }
+}
